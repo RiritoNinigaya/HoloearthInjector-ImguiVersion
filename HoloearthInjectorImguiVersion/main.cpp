4b42997b914d723f934946487302214f08de55f3
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-04-01 23:35:41 +0300

    Update main.cpp

--- HoloearthInjectorImguiVersion/main.cpp
+++ HoloearthInjectorImguiVersion/main.cpp
@@ -14,7 +14,6 @@ bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
 void CreateRenderTarget();
 void CleanupRenderTarget();
-void DllManualMap(Process& proc, const wstring& path);
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 int width;
 int height;
